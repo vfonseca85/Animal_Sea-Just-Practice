2d29ae5b929feb93497b15ff01e797399b1730f8
Valdir Fonseca <valdirkleiton@gmail.com>
2023-05-27 16:45:10 +0100

    Index File Added

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,54 @@
 import { animals } from './animals';
 import {React, createRoot} from 'react-dom/client'
-const container = document.getElementById('app');
-const root = createRoot(container);
 
-const title = " ";
 
-const background = 
-    <img className="background" alt="ocean" src="/images/ocean.jpg" />;
+function App()
+{
 
-const images = [];
-for(const animal in animals){
-  const image = (
-    <img 
-    key= {animal}
-    className= "animal"
-    alt= {animal}
-    src={animals[animal].image}
-    aria-label= {animal}
-    role='button'
-    onClick={displayFact}
-    />
-  );
-  images.push(image);
-}
+  const title = " ";
 
-const showBackground = true;
+  const background = 
+      <img className="background" alt="ocean" src="/images/ocean.jpg" />;
 
-const animalFacts = (
-  <div>
-    <h1>{title === " " ? "Click an animal for a fun fact" : title}</h1>
-    {showBackground && background}
-    <p id="fact"></p>
-    <div className="animals">
-      {images}
+  const images = [];
+  for(const animal in animals){
+    const image = (
+      <img 
+      key= {animal}
+      className= "animal"
+      alt= {animal}
+      src={animals[animal].image}
+      aria-label= {animal}
+      role='button'
+      onClick={displayFact}
+      />
+    );
+    images.push(image);
+  }
+
+  const showBackground = true;
+
+  const animalFacts = (
+    <div>
+      <h1>{title === " " ? "Click an animal for a fun fact" : title}</h1>
+      {showBackground && background}
+      <p id="fact"></p>
+      <div className="animals">
+        {images}
+      </div>
     </div>
-  </div>
-);
+  );
 
-function displayFact (e) {
-  const animal = e.target.alt;
-  const index = Math.floor(Math.random() * animals[animal].facts.length);
-  const funFact = animals[animal].facts[index];
-  const p = document.getElementById("fact");
-  p.innerHTML = funFact;
+  function displayFact (e) {
+    const animal = e.target.alt;
+    const index = Math.floor(Math.random() * animals[animal].facts.length);
+    const funFact = animals[animal].facts[index];
+    const p = document.getElementById("fact");
+    p.innerHTML = funFact;
+  }
+
+  animalFacts();
+
 }
 
-root.render(animalFacts);
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+import App from 'App';
+
+
+root.render(<App />);
